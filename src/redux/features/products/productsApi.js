7b0e728e5d04633cd1e7ff7c2f2938bca35dbe8b
shoriful-dev <cmshohelrana50@gmail.com>
2025-11-14 13:08:29 +0600

    add useFetchAllProductsQuery

---
+++ src/redux/features/products/productsApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getBaseURL } from '../../../utils/getBaseURL';
+
+const productsApi = createApi({
+  reducerPath: 'productsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${getBaseURL()}/api/product`,
+    credentials: 'include',
+  }),
+  endpoints: (builder) => ({
+    fetchAllProducts: builder.query({
+      query: ({category, color, minPrice, maxPrice, page=1, limit=10}) => {
+        const queryParams = new URLSearchParams({
+          category: category || '',
+          color: color || '',
+          minPrice: minPrice || 0,
+          maxPrice: maxPrice || '',
+          page: page.toString(),
+          limit: limit.toString()
+        })
+        return `/?${queryParams}`
+      }
+    })
+  })
+});
+
+export const { useFetchAllProductsQuery } = productsApi;
