07c8ab7e31ae9547c8dc7ded78f30969ba344e33
shoriful-dev <cmshohelrana50@gmail.com>
2025-11-17 20:48:34 +0600

    all set

--- src/redux/features/products/productsApi.js
+++ src/redux/features/products/productsApi.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { getBaseURL } from '../../../utils/getBaseURL';
-
-const productsApi = createApi({
-  reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${getBaseURL()}/api/product`,
-    credentials: 'include',
-  }),
-  
-  endpoints: (builder) => ({
-    fetchAllProducts: builder.query({
-      query: ({category, color, minPrice, maxPrice, page=1, limit=10}) => {
-        const queryParams = new URLSearchParams({
-          category: category || '',
-          color: color || '',
-          minPrice: minPrice || 0,
-          maxPrice: maxPrice || '',
-          page: page.toString(),
-          limit: limit.toString()
-        })
-        return `/?${queryParams}`
-      }
-    }),
-    fetchProductById: builder.query({
-      
-    })
-  })
-});
-
-export const { useFetchAllProductsQuery } = productsApi;
-export default productsApi;
-=======
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getBaseURL } from '../../../utils/getBaseURL';
 
@@ -63,4 +29,3 @@ const productsApi = createApi({
 
 export const { useFetchAllProductsQuery } = productsApi;
 export default productsApi;
->>>>>>> 9d6cf27680a07552898b813010134684262a026d
