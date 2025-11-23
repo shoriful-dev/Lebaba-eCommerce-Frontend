79e598a1469f7fca415e2a4c830e004e725621a1
shoriful-dev <cmshohelrana50@gmail.com>
2025-11-23 17:02:36 +0600

    fixed origin

--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getBaseURL } from '../utils/getBaseURL';
 
 const PaymentSuccess = () => {
-  return (
-    <div>PaymentSuccess</div>
-  )
-}
+  const [order, setOrder] = useState(null);
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    const sessionId = query.get('session_id');
+    if (sessionId) {
+      const confimedPayment = async () => {
+        const reponse = await axios.post(
+          `${getBaseURL()}/api/orders/confirm-payment`,
+          {
+            session_id: sessionId,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
+
+        if (reponse?.data) {
+          setOrder(reponse?.data.data);
+        }
+      };
+      confimedPayment();
+    }
+  }, []);
+console.log(order)
+  return <div>PaymentSuccess</div>;
+};
 
 export default PaymentSuccess;
--- src/utils/getBaseURL.js
+++ src/utils/getBaseURL.js
@@ -1,3 +1,7 @@
+// export const getBaseURL = () => {
+//   return 'https://lebaba-ecommerce-backend.onrender.com';
+// };
+
 export const getBaseURL = () => {
-  return 'https://lebaba-ecommerce-backend.onrender.com';
+  return 'http://localhost:5173';
 };
